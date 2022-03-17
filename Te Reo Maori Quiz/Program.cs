d26fda52bb5afa37cf015d80420c4ba479c5b6cd
Muhammad Amar <ac116681@avcol.school.nz>
2022-03-17 15:14:33 +1300

    added go to easy level added one question

--- Te Reo Maori Quiz/Program.cs
+++ Te Reo Maori Quiz/Program.cs
@@ -1,23 +1,33 @@
-﻿//This program is a Te Reo Maori quiz
-Console.Write("This is a Te Reo Maori Quiz! \nPress any key to start!");
-Console.ReadKey();
+﻿//This program is a Te Reo Maori quiz, created by Muhammad Amar
+Console.Write("This is a Te Reo Maori Quiz! \nPlease start your answers with captial letters for every word \nPress any key to start!");
+Console.ReadKey(); //waits for user to input key
 Console.WriteLine("\n");
-Console.WriteLine("Would you like an easy, medium, or hard level?");
+Console.WriteLine("Would you like an Easy, Medium, or Hard level?");
 string level = Console.ReadLine();
-if (level == "easy")
+if (level == "Easy")
 {
     Console.WriteLine("Great you selected easy!");
-    
+    goto easy;
+         
 }
-else if (level == "medium")
+else if (level == "Medium")
 {
     Console.WriteLine("Great you selected medium!");
 }
-else if (level == "hard")
+else if (level == "Hard")
 {
     Console.WriteLine("Prepare youself for the hardest thing in your life!");
 }
 else
 {
-    Console.WriteLine("Please enter your level again and DONT use uppercase letters");
+    Console.WriteLine("Please enter your level again and use uppercase letters");
+}
+
+
+easy:
+Console.WriteLine("How do you say well done in Maori?");
+Console.Write("A) Kia Ora, B) Ka Pai, C) Taniwha? ");
+if (Console.ReadLine() == "Kia Ora")
+{
+    Console.WriteLine("Ka Pai! You got it correct");
 }
