136a3d2ddbdbed12a1cece654698441014bb24c1
Muhammad Amar <ac116681@avcol.school.nz>
2022-03-21 11:50:38 +1300

    replaced goto with method.

--- Te Reo Maori Quiz/Program.cs
+++ Te Reo Maori Quiz/Program.cs
@@ -1,33 +1,64 @@
 ﻿//This program is a Te Reo Maori quiz, created by Muhammad Amar
-Console.Write("This is a Te Reo Maori Quiz! \nPlease start your answers with captial letters for every word \nPress any key to start!");
-Console.ReadKey(); //waits for user to input key
-Console.WriteLine("\n");
-Console.WriteLine("Would you like an Easy, Medium, or Hard level?");
-string level = Console.ReadLine();
-if (level == "Easy")
+using System;
+
+namespace methods
 {
-    Console.WriteLine("Great you selected easy!");
-    goto easy;
-         
-}
-else if (level == "Medium")
-{
-    Console.WriteLine("Great you selected medium!");
-}
-else if (level == "Hard")
-{
-    Console.WriteLine("Prepare youself for the hardest thing in your life!");
-}
-else
-{
-    Console.WriteLine("Please enter your level again and use uppercase letters");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is a Te Reo Maori Quiz! \nPress any key to start!");
+            Console.ReadKey(); //waits for user to input key
+            Console.WriteLine("\n");
+            Console.WriteLine("Would you like an easy, medium, or hard level?");
+            string level = Console.ReadLine().ToLower();
+
+            switch (level)
+            {
+                case "easy":
+                    Console.WriteLine("Great you selected easy!");
+                    MethodEasy();
+                    break;
+                case "medium":
+                    Console.WriteLine("Great you selected medium!");
+                    break;
+                case "hard":
+                    Console.WriteLine("Prepare youself for the hardest thing in your life!");
+                    break;
+                default:
+                    Console.WriteLine("Please enter your level again and use uppercase letters");
+                    break;
+            }
+        }
+        static void MethodEasy()
+        {
+            Q1E:
+            Console.WriteLine("How do you say well done in Maori?");
+            Console.Write("A) Kia Ora, B) Ka Pai, C) Taniwha? ");
+            if (Console.ReadLine().ToLower() == "ka pai")
+            {
+                Console.WriteLine("Ka Pai! You got it correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect! Try again?");
+                string again1 = Console.ReadLine().ToLower();
+                while (again1 == "yes")
+                {
+                    goto Q1E;
+                }
+            }
+            Q2E:
+
+        }
+
+    }
+
 }
 
 
-easy:
-Console.WriteLine("How do you say well done in Maori?");
-Console.Write("A) Kia Ora, B) Ka Pai, C) Taniwha? ");
-if (Console.ReadLine() == "Kia Ora")
-{
-    Console.WriteLine("Ka Pai! You got it correct");
-}
+
+
+
+
+
